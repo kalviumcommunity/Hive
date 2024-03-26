48679030bb4f81c79188f901d5c5a83d568bc750
AlexCFrost <siva.ganesh.c18@gmail.com>
2024-03-26 15:37:24 +0530

    Connected MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose')
 const PORT = process.env.PORT
 
-app.get('/', (req,res) => {
-    res.send("Hive is Now Online")
-})
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+
+const app = express();
+app.use(express.json());
+
+
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+  });
+
+mongoose
+  .connect(process.env.mongoUri)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to db & Api is running on port`, process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
